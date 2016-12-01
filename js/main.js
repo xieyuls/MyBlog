8d8ff1d5fd14f035420bad73f6c61c2df9648c77
xieyuls <410880162@qq.com>
2016-12-01 20:02:03 +0800

    用js在第一次时读取浏览器窗口大小以作响应式布局

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 /**
  * Created by zjf on 2016/12/1.
  */
+
+var height=$(window).height(); 
+
+window.onload = function() {
+	$(".wrapper").css("height",(height)*2);
+	$(".intro").css("height",height);
+	$(".intro_button").css("top",0.8*height);
+    $(".content_button").css("top",0.2*height);
+}
+
 $(".intro_button").click(function(){
-    $('html,body').animate({scrollTop: '800px'}, 800);
-    $(".content_button img").fadeIn(3500);
+    $('html,body').animate({scrollTop: height}, 800);
+    console.log($(window).height());
+    console.log($(".intro").css("height"));
 });
 
 $(".content_button").click(function(){
     $('html,body').animate({scrollTop: '0px'}, 800);
-    $(".intro_img").fadeIn(3500);
+    console.log(height);
 });
