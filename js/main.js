5778ae1e96901531e3d0213cfefe79c2f36b075e
xieyuls <410880162@qq.com>
2016-12-01 22:24:44 +0800

    解决加载问题

--- js/main.js
+++ js/main.js
@@ -4,23 +4,15 @@
 
 
 var height=$(window).height(); 
-
-window.onload = function() {
-	$(".wrapper").css("height",(height)*2);
-	$(".intro").css("height",height);
-	$(".intro_button").css("top",0.8*height);
-    $(".content_button").css("top",0.2*height);
-    var image=new Image();
-    img.src="../img/icon/icon1.jpg";
-}
+$(".wrapper").css("height",(height)*2);
+$(".intro").css("height",height);
+$(".intro_button").css("top",0.8*height);
+$(".content_button").css("top",0.2*height);
 
 $(".intro_button").click(function(){
     $('html,body').animate({scrollTop: height}, 800);
-    console.log($(window).height());
-    console.log($(".intro").css("height"));
 });
 
 $(".content_button").click(function(){
     $('html,body').animate({scrollTop: '0px'}, 800);
-    console.log(height);
 });
