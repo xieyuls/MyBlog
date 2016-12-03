9f6e972b0f28dfe28d96c5097ca66ac09653712b
xieyuls <410880162@qq.com>
2016-12-03 18:01:42 +0800

    让内容栏大小自适应窗口

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@
  */
 
 
-var height=$(window).height(); 
-
+var height=$(window).height();
+var width=$(window).width();  
 $(".wrapper").css("height",(height)*2);
 $(".intro").css("height",height);
 $(".intro_button").css("top",0.8*height);
 $(".content_button").css("top",0.2*height);
-
+$(".content1").css({"height":0.4*height,"width":0.4*width});
+$(".content2").css({"height":0.4*height,"width":0.4*width});
 $(".intro_button").click(function(){
     $('html,body').animate({scrollTop: height}, 800);
 });
