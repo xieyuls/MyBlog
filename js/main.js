8d21ebb1a32cc9aabb0210191daed13542324cdc
xieyuls <410880162@qq.com>
2016-12-04 20:26:29 +0800

    修改上下箭头的位置偏移问题

--- js/main.js
+++ js/main.js
@@ -7,15 +7,19 @@ var height=$(window).height();
 var width=$(window).width();  
 $(".wrapper").css("height",(height)*2);
 $(".intro").css("height",height);
-$(".intro_button").css("top",0.8*height);
-$(".content_button").css("top",0.2*height);
+$(".down,.down img").css("top",0.8*height);
+$(".up,.up img").css("top",0.2*height);
+
 $(".content1").css({"height":0.4*height,"width":0.4*width});
 $(".content2").css({"height":0.4*height,"width":0.4*width});
-$(".intro_button").click(function(){
-    $('html,body').animate({scrollTop: height}, 800);
+
+$(".up").click(function(){
+    $('html,body').animate({scrollTop: "0px"}, 800);
+});
+$(".down").click(function(){
+	
+    $('html,body').animate({scrollTop: height+"px"}, 800);
 });
 
-$(".content_button").click(function(){
-    $('html,body').animate({scrollTop: '0px'}, 800);
-});
+
 
