2222960fafce18e07a589719eb72be1d5a0df3ef
xieyuls <410880162@qq.com>
2016-12-05 16:40:52 +0800

    大量修改
    1.将箭头改成了圆形选区按钮
    2.添加了第三层的Echarts
    3.更换了首页图片

--- js/main.js
+++ js/main.js
@@ -2,24 +2,58 @@
  * Created by zjf on 2016/12/1.
  */
 
-
+//初始化时读取浏览器可用高度及宽度，设置图片大小及位置
 var height=$(window).height();
-var width=$(window).width();  
-$(".wrapper").css("height",(height)*2);
+var width=$(window).width();
+var count=0;
+$(".wrapper").css("height",(height)*3);
 $(".intro").css("height",height);
-$(".down,.down img").css("top",0.8*height);
-$(".up,.up img").css("top",0.2*height);
+$(".content").css("height",height);
+$("#map").css("height",height);
+// $(".down,.down img").css("top",0.8*height);
+// $(".up,.up img").css("top",0.2*height);
+$(".select").css("top",0.5*height);
+$('html,body').animate({scrollTop: '0px'}, 800);
+$(".select1").css("opacity",1);
+$(".content1,.content2").css({"height":0.4*height,"width":0.4*width});
+$(".content1 img,.content2 img").css({"height":0.32*height,"width":0.38*width});
 
-$(".content1").css({"height":0.4*height,"width":0.4*width});
-$(".content2").css({"height":0.4*height,"width":0.4*width});
 
-$(".up").click(function(){
-    $('html,body').animate({scrollTop: "0px"}, 800);
+//上下箭头事件
+// $(".up").click(function(){
+//     if(count==0) count=0;
+//     else count=count-1;
+//     console.log($('.content').offset());
+//     $('html,body').animate({scrollTop: '0px'}, 800);
+// });
+// $(".down").click(function(){
+//     if(count==2) count=2;
+//     else count=count+1;
+//     console.log($('.content').offset());
+//     $('html,body').animate({scrollTop: height}, 800);
+// });
+
+//界面跳转选择
+$(".select1").click(function(){
+    $('html,body').animate({scrollTop: '0px'}, 800);
+    $(".select1").css("opacity",1);
+    $(".select2").css("opacity",0.2);
+    $(".select3").css("opacity",0.2);
+
 });
-$(".down").click(function(){
-	
-    $('html,body').animate({scrollTop: height+"px"}, 800);
+$(".select2").click(function(){
+    $('html,body').animate({scrollTop: height}, 800);
+    $(".select1").css("opacity",0.2);
+    $(".select2").css("opacity",1);
+    $(".select3").css("opacity",0.2);
 });
+$(".select3").click(function(){
+    $('html,body').animate({scrollTop: 2*height}, 800);
+    $(".select1").css("opacity",0.2);
+    $(".select2").css("opacity",0.2);
+    $(".select3").css("opacity",1);
+});
+
 
 
 
